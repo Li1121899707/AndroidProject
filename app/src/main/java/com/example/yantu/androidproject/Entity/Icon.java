94c4d1286016eba7e1a14b1a09c940fd6e300f28
zangbohao <1922267871@qq.com>
2019-05-05 21:08:46 +0800

    修改数据库

---
+++ app/src/main/java/com/example/yantu/androidproject/Entity/Icon.java
@@ -0,0 +1,23 @@
+package com.example.yantu.androidproject.Entity;
+
+public class Icon {
+
+    private int icId;
+    private String icPath;
+    public int getIcId() {
+        return icId;
+    }
+
+    public void setIcId(int icId) {
+        this.icId = icId;
+    }
+
+    public String getIcPath() {
+        return icPath;
+    }
+
+    public void setIcPath(String icPath) {
+        this.icPath = icPath;
+    }
+
+}
