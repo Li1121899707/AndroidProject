97e29aca239c23a9d45f14ef6aadbded81b25ead
Gitforyaoyue <1015205448@qq.com>
2019-05-06 17:29:30 +0800

    完成了首页基本界面，实现了倒计时计算

--- app/src/main/java/com/example/yantu/androidproject/Adapter/DailyRecycleAdapter.java
+++ app/src/main/java/com/example/yantu/androidproject/Adapter/DailyRecycleAdapter.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -48,3 +49,4 @@ public class DailyRecycleAdapter extends RecyclerView.Adapter<DailyRecycleAdapte
 
 
 }
+
