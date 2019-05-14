dce0697739b2353eda4012d342bc93487b770b6d
lxdlam <lxdlam@gmail.com>
2019-05-14 22:55:04 +0800

    [Update] Finish TomatoClockActivity.

--- app/src/main/java/com/example/yantu/androidproject/TomatoClockActivity.java
+++ app/src/main/java/com/example/yantu/androidproject/TomatoClockActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -227,5 +228,12 @@ public class TomatoClockActivity extends AppCompatActivity {
 
         init();
         changeState();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK)
+            return true;
+        return super.onKeyDown(keyCode, event);
     }
 }
